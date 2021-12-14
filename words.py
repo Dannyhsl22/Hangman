cc9fae62359b75658e6c893a04c75f190ed0a4e6
Dannyhsl22 <dannyhawasli@gmail.com>
2021-12-14 13:07:42 +0000

    create word list

---
+++ words.py
@@ -0,0 +1,9 @@
+word_list = [
+    "Console", "variable", "operator", "float", 'boolean', 'string', 'loop',
+    "list", 'integer', 'append', 'insert', 'pop', 'parameter', "else",
+    "False", "await", "else", "import", "pass", "None", "break",
+    "except", "in", "raise", "True", 'class', 'finally', 'is',
+    'return', 'and', 'continue', 'for', 'try', 'def', 'from', 'while',
+    'assert', 'global', 'not', 'with', 'async', 'elif', 'if', 'or',
+    'yield', 'print', 'import', 'random'
+]
